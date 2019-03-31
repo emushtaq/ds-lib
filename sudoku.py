fbbe5b6283a4ee0c8548a93d41fccfa24e5de263
emushtaq <eshanmushtaq@gmail.com>
2019-03-31 14:38:13 +0200

    added validation methods

--- sudoku.py
+++ sudoku.py
@@ -1,3 +1,4 @@
+import numpy as np
 """
 A Sudoku solver
 """
@@ -39,29 +40,22 @@ Sample Output:
 7 3 4 | 9 6 8 | 5 2 1
 """
 
+solution = [[9, 4, 7, 6, 8, 2, 1, 3, 5],
+            [1, 5, 8, 4, 7, 3, 2, 6, 9],
+            [2, 6, 3, 5, 1, 9, 8, 4, 7],
+            [3, 9, 2, 1, 4, 7, 6, 5, 8],
+            [8, 1, 6, 3, 2, 5, 7, 9, 4],
+            [4, 7, 5, 8, 9, 6, 3, 1, 2],
+            [6, 2, 1, 7, 5, 4, 9, 8, 3],
+            [5, 8, 9, 2, 3, 1, 4, 7, 6],
+            [7, 3, 4, 9, 6, 8, 5, 2, 1]]
+
 
 def getInput():
     input = input()
     if input == "*":
         return None
     return input
-
-
-print("-----------  SUDOKU SOLVER -----------")
-# print(" Enter the inputs for the puzzle. Enter '*' when there are no inputs")
-# TODO sudoku = [[input() for i in range(9)] for j in range(9)]
-
-sudoku = [
-    ["*", "*", "7", "*", "8", "*", "*", "3", "*"],
-    ["*", "*", "*", "*", "*", "*", "2", "6", "9"],
-    ["*", "6", "*", "*", "1", "9", "*", "*", "7"],
-    ["*", "9", "2", "*", "4", "*", "*", "*", "*"],
-    ["8", "*", "*", "3", "*", "5", "*", "*", "4"],
-    ["*", "*", "*", "*", "9", "*", "3", "1", "*"],
-    ["6", "*", "*", "7", "5", "*", "*", "8", "*"],
-    ["*", "8", "9", "*", "*", "*", "*", "*", "*"],
-    ["*", "3", "*", "*", "6", "*", "5", "*", "*"]
-]
 
 
 def convert_input(value):
@@ -71,9 +65,68 @@ def convert_input(value):
         return int(value)
 
 
-print("Solving the puzzle : ")
-sudoku = [[convert_input(value) for value in line] for line in sudoku]
+def is_valid_set(values):
+    return len(set(values)) == len(values)
 
-for line in sudoku:
-    print(line)
-    # print(" ".join(str(line)))
+
+def are_rows_valid(sudoku):
+    for row in sudoku:
+        if (0 in row) or not is_valid_set(row):
+            print(f"Row {row} is invalid.")
+            return False
+    return True
+
+
+def are_cols_valid(sudoku):
+    for col in np.transpose(sudoku):
+        if (0 in col) or not is_valid_set(col):
+            print(f"Col {col} is invalid.")
+            return False
+    return True
+
+
+def are_cells_valid(sudoku):
+    cells = []
+    offset = 3
+    for i in range(0, len(sudoku), 3):  # row cell offset
+        for j in range(0, len(sudoku), 3):  # col cell offset
+            cells.append(sudoku[i: i + offset, j: j + offset].reshape(-1))
+    
+    for cell in cells:
+        if (0 in cell) or not is_valid_set(cell):
+            print(f"Cell {cell} is invalid.")
+            return False
+
+    return True
+
+
+if __name__ == "__main__":
+    print("-----------  SUDOKU SOLVER -----------")
+    # print(" Enter the inputs for the puzzle. Enter '*' when there are no inputs")
+    # TODO sudoku = [[input() for i in range(9)] for j in range(9)]
+
+    sudoku = [
+        ["*", "*", "7", "*", "8", "*", "*", "3", "*"],
+        ["*", "*", "*", "*", "*", "*", "2", "6", "9"],
+        ["*", "6", "*", "*", "1", "9", "*", "*", "7"],
+        ["*", "9", "2", "*", "4", "*", "*", "*", "*"],
+        ["8", "*", "*", "3", "*", "5", "*", "*", "4"],
+        ["*", "*", "*", "*", "9", "*", "3", "1", "*"],
+        ["6", "*", "*", "7", "5", "*", "*", "8", "*"],
+        ["*", "8", "9", "*", "*", "*", "*", "*", "*"],
+        ["*", "3", "*", "*", "6", "*", "5", "*", "*"]
+    ]
+
+    print("Solving the puzzle : ")
+    sudoku = [[convert_input(value) for value in line] for line in sudoku]
+
+    sudoku, solution = np.array(sudoku), np.array(solution)
+
+    # for line in sudoku:
+    #     print(line)
+
+    # print("Validity : ", are_rows_valid(sudoku))
+    # print("Validity : ", are_cols_valid(sudoku))
+    print("Validity : ", are_cells_valid(sudoku))
+
+    pass
